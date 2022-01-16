3c8ff9d52ff6d4608825f24e62de8a8742e2031c
Sabiri-Abderrahman <75841796+Sabiri-Abderrahman@users.noreply.github.com>
2022-01-16 14:24:11 +0100

    Add files via upload

---
+++ toDoApp/toDoApp/EntryViewController.swift
@@ -0,0 +1,43 @@
+//
+//  EntryViewController.swift
+//  toDoApp
+//
+//  Created by abderrahman  on 09/01/2022.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet var field:UITextField!
+    @objc dynamic var date: Date = Date()
+    
+    var update:(() -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        field.delegate = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveTask))
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        saveTask()
+        return true
+    }
+
+    @objc func saveTask() {
+        guard let text = field.text, !text.isEmpty else {
+            return
+        }
+        guard let count = UserDefaults().value(forKey: "count") as? Int else { return  }
+        let newCount = count + 1
+        UserDefaults().set(newCount, forKey: "count")
+        UserDefaults().set(text, forKey: "task_\(newCount)")
+        
+        update?()
+        
+        navigationController?.popViewController(animated: true)
+    }
+}
